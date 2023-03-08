9eebc8c8f2ed41d5d506f957d4c51296bdc6da15
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-03-08 18:57:07 -0300

    constrain min and max brightness

--- lib/live_studio_web/live/light_live.ex
+++ lib/live_studio_web/live/light_live.ex
@@ -37,12 +37,12 @@ defmodule LiveStudioWeb.LightLive do
   end
 
   def handle_event("up", _, socket) do
-    socket = update(socket, :brightness, &(&1 + 10))
+    socket = update(socket, :brightness, &min(&1 + 10, 100))
     {:noreply, socket}
   end
 
   def handle_event("down", _, socket) do
-    socket = update(socket, :brightness, &(&1 - 10))
+    socket = update(socket, :brightness, &max(&1 - 10, 0))
     {:noreply, socket}
   end
 
