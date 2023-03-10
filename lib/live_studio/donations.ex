f4a0b00ce8cbcf2df3901be89c7ae44dbcd9c2b7
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-03-10 14:28:18 -0300

    generate donations context

---
+++ lib/live_studio/donations.ex
@@ -0,0 +1,123 @@
+defmodule LiveStudio.Donations do
+  @moduledoc """
+  The Donations context.
+  """
+
+  import Ecto.Query, warn: false
+  alias LiveStudio.Repo
+
+  alias LiveStudio.Donations.Donation
+
+  @doc """
+  Returns the list of donations.
+
+  ## Examples
+
+      iex> list_donations()
+      [%Donation{}, ...]
+
+  """
+  def list_donations do
+    Repo.all(from d in Donation, order_by: [asc: d.id])
+  end
+
+  @doc """
+  Returns a list of donations based on the given `options`.
+
+  Example options:
+
+  %{sort_by: :item, sort_order: :asc}
+  """
+  def list_donations(options) when is_map(options) do
+    from(Donation)
+    |> sort(options)
+    |> Repo.all()
+  end
+
+  defp sort(query, %{sort_by: sort_by, sort_order: sort_order}) do
+    order_by(query, {^sort_order, ^sort_by})
+  end
+
+  defp sort(query, _options), do: query
+
+  @doc """
+  Gets a single donation.
+
+  Raises `Ecto.NoResultsError` if the Donation does not exist.
+
+  ## Examples
+
+      iex> get_donation!(123)
+      %Donation{}
+
+      iex> get_donation!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_donation!(id), do: Repo.get!(Donation, id)
+
+  @doc """
+  Creates a donation.
+
+  ## Examples
+
+      iex> create_donation(%{field: value})
+      {:ok, %Donation{}}
+
+      iex> create_donation(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_donation(attrs \\ %{}) do
+    %Donation{}
+    |> Donation.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a donation.
+
+  ## Examples
+
+      iex> update_donation(donation, %{field: new_value})
+      {:ok, %Donation{}}
+
+      iex> update_donation(donation, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_donation(%Donation{} = donation, attrs) do
+    donation
+    |> Donation.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a donation.
+
+  ## Examples
+
+      iex> delete_donation(donation)
+      {:ok, %Donation{}}
+
+      iex> delete_donation(donation)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_donation(%Donation{} = donation) do
+    Repo.delete(donation)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking donation changes.
+
+  ## Examples
+
+      iex> change_donation(donation)
+      %Ecto.Changeset{data: %Donation{}}
+
+  """
+  def change_donation(%Donation{} = donation, attrs \\ %{}) do
+    Donation.changeset(donation, attrs)
+  end
+end
