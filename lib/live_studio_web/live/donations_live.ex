f6f669e1f0af191a9bd966ae31c7cb71caa173b8
viniLataro <viniciusr.lataro@gmail.com>
2023-03-11 20:21:54 -0300

    add sort link component and make it work

--- lib/live_studio_web/live/donations_live.ex
+++ lib/live_studio_web/live/donations_live.ex
@@ -4,13 +4,47 @@ defmodule LiveStudioWeb.DonationsLive do
   alias LiveStudio.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
+    {:ok, socket, temporary_assigns: [donations: []]}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    donations = Donations.list_donations(options)
 
     socket =
       assign(socket,
-        donations: donations
+        donations: donations,
+        options: options
       )
 
-    {:ok, socket}
+    {:noreply, socket}
+  end
+
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+  slot :inner_block, required: true
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
   end
 end
