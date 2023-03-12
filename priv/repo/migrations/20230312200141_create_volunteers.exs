bf3a94fc192b5f302e61eb77b76d5d1dae7a35eb
viniLataro <viniciusr.lataro@gmail.com>
2023-03-12 19:19:43 -0300

    generate volunteers context

---
+++ priv/repo/migrations/20230312200141_create_volunteers.exs
@@ -0,0 +1,13 @@
+defmodule LiveStudio.Repo.Migrations.CreateVolunteers do
+  use Ecto.Migration
+
+  def change do
+    create table(:volunteers) do
+      add :name, :string
+      add :phone, :string
+      add :checked_out, :boolean, default: false, null: false
+
+      timestamps()
+    end
+  end
+end
