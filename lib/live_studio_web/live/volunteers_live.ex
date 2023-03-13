e0006fdd9cba0b1651148f803fbd1d3ab64f195f
viniLataro <viniciusr.lataro@gmail.com>
2023-03-13 12:01:25 -0300

    add live validation

--- lib/live_studio_web/live/volunteers_live.ex
+++ lib/live_studio_web/live/volunteers_live.ex
@@ -22,9 +22,15 @@ defmodule LiveStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
-      <.form for={@form} phx-submit="save">
-        <.input field={@form[:name]} placeholder="Name" autocomplete="off" />
-        <.input field={@form[:phone]} type="tel" placeholder="Phone" autocomplete="off" />
+      <.form for={@form} phx-submit="save" phx-change="validate">
+        <.input field={@form[:name]} placeholder="Name" autocomplete="off" phx-debounce="2000" />
+        <.input
+          field={@form[:phone]}
+          type="tel"
+          placeholder="Phone"
+          autocomplete="off"
+          phx-debounce="blur"
+        />
         <.button phx-disable-with="Saving...">
           Check In
         </.button>
@@ -49,6 +55,15 @@ defmodule LiveStudioWeb.VolunteersLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("validate", %{"volunteer" => volunteer_params}, socket) do
+    changeset =
+      %Volunteer{}
+      |> Volunteers.change_volunteer(volunteer_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_form(socket, changeset)}
   end
 
   def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
