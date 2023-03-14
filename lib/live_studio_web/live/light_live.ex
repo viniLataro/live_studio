304b4a9aa7d943bf699c11b80553db24797837a4
viniLataro <viniciusr.lataro@gmail.com>
2023-03-14 16:29:53 -0300

    refactor light live using function components

--- lib/live_studio_web/live/light_live.ex
+++ lib/live_studio_web/live/light_live.ex
@@ -10,47 +10,62 @@ defmodule LiveStudioWeb.LightLive do
     ~H"""
     <h1>Front Porch Light</h1>
     <div id="light">
-      <div class="meter">
-        <span style={"width: #{@brightness}%; background: #{temp_color(@temp)}"}>
-          <%= @brightness %>%
-        </span>
-      </div>
-      <button phx-click="off">
-        <img src="/images/light-off.svg" />
-      </button>
-      <button phx-click="down">
-        <img src="/images/down.svg" />
-      </button>
-      <button phx-click="rando">
-        <img src="/images/fire.svg" />
-      </button>
-      <button phx-click="up">
-        <img src="/images/up.svg" />
-      </button>
-      <button phx-click="on">
-        <img src="/images/light-on.svg" />
-      </button>
-      <form phx-change="slide">
-        <input
-          type="range"
-          min="0"
-          max="100"
-          name="brightness"
-          value={@brightness}
-          phx-debounce="250"
-        />
-      </form>
-      <form phx-change="change-temp">
-        <div class="temps">
-          <%= for temp <- ["3000", "4000", "5000"] do %>
-            <div>
-              <input type="radio" id={temp} name="temp" value={temp} checked={temp == @temp} />
-              <label for="temp"><%= temp %></label>
-            </div>
-          <% end %>
-        </div>
-      </form>
+      <.meter brightness={@brightness} temp={@temp} />
+      <.button_control />
+      <.slide_control brightness={@brightness} phx-debounce="250" />
+      <.temp_control temp={@temp} />
     </div>
+    """
+  end
+
+  attr :brightness, :integer, required: true
+  attr :temp, :string, required: true
+
+  def meter(assigns) do
+    ~H"""
+    <div class="meter">
+      <span style={"width: #{@brightness}%; background: #{temp_color(@temp)}"}>
+        <%= @brightness %>%
+      </span>
+    </div>
+    """
+  end
+
+  def button_control(assigns) do
+    ~H"""
+    <button phx-click="off"><img src="/images/light-off.svg" /></button>
+    <button phx-click="down"><img src="/images/down.svg" /></button>
+    <button phx-click="rando"><img src="/images/fire.svg" /></button>
+    <button phx-click="up"><img src="/images/up.svg" /></button>
+    <button phx-click="on"><img src="/images/light-on.svg" /></button>
+    """
+  end
+
+  attr :brightness, :integer, required: true
+  attr :rest, :global
+
+  def slide_control(assigns) do
+    ~H"""
+    <form phx-change="slide">
+      <input type="range" min="0" max="100" name="brightness" value={@brightness} {@rest} />
+    </form>
+    """
+  end
+
+  attr :temp, :string, required: true
+
+  def temp_control(assigns) do
+    ~H"""
+    <form phx-change="change-temp">
+      <div class="temps">
+        <%= for temp <- ["3000", "4000", "5000"] do %>
+          <div>
+            <input type="radio" id={temp} name="temp" value={temp} checked={temp == @temp} />
+            <label for="temp"><%= temp %></label>
+          </div>
+        <% end %>
+      </div>
+    </form>
     """
   end
 
