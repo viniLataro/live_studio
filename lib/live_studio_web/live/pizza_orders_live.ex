79214e1fd28e54c66acfde631de3480626575468
viniLataro <viniciusr.lataro@gmail.com>
2023-03-15 15:34:36 -0300

    add sort live pizza orders page

---
+++ lib/live_studio_web/live/pizza_orders_live.ex
@@ -0,0 +1,47 @@
+defmodule LiveStudioWeb.PizzaOrdersLive do
+  use LiveStudioWeb, :live_view
+
+  alias LiveStudio.PizzaOrders
+  import Number.Currency
+
+  def mount(_params, _session, socket) do
+    {:ok, socket, temporary_assigns: [pizza_orders: []]}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    pizza_orders = PizzaOrders.list_pizza_orders(options)
+
+    socket =
+      assign(socket,
+        pizza_orders: pizza_orders,
+        options: options
+      )
+
+    {:noreply, socket}
+  end
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/pizza-orders?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
+  end
+end
