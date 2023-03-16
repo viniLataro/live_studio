7110a79b5c548ed22154d8c7d60c29245133abeb
viniLataro <viniciusr.lataro@gmail.com>
2023-03-16 19:38:35 -0300

    add sort indicator and params validation

--- lib/live_studio_web/live/pizza_orders_live.ex
+++ lib/live_studio_web/live/pizza_orders_live.ex
@@ -9,8 +9,8 @@ defmodule LiveStudioWeb.PizzaOrdersLive do
   end
 
   def handle_params(params, _uri, socket) do
-    sort_by = (params["sort_by"] || "id") |> String.to_atom()
-    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+    sort_by = valid_sort_by(params)
+    sort_order = valid_sort_order(params)
 
     options = %{
       sort_by: sort_by,
@@ -28,15 +28,30 @@ defmodule LiveStudioWeb.PizzaOrdersLive do
     {:noreply, socket}
   end
 
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+  slot :inner_block, required: true
+
   def sort_link(assigns) do
     ~H"""
     <.link patch={
       ~p"/pizza-orders?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
     }>
       <%= render_slot(@inner_block) %>
+      <%= sort_indicator(@sort_by, @options) %>
     </.link>
     """
   end
+
+  defp sort_indicator(column, %{sort_by: sort_by, sort_order: sort_order})
+       when column == sort_by do
+    case sort_order do
+      :asc -> "👆"
+      :desc -> "👇"
+    end
+  end
+
+  defp sort_indicator(_, _), do: ""
 
   defp next_sort_order(sort_order) do
     case sort_order do
@@ -44,4 +59,17 @@ defmodule LiveStudioWeb.PizzaOrdersLive do
       :desc -> :asc
     end
   end
+
+  defp valid_sort_by(%{"sort_by" => sort_by})
+       when sort_by in ~w(id size style topping_1 topping_2 price) do
+    String.to_atom(sort_by)
+  end
+
+  defp valid_sort_by(_params), do: :id
+
+  defp valid_sort_order(%{"sort_order" => sort_order}) when sort_order in ~w(asc desc) do
+    String.to_atom(sort_order)
+  end
+
+  defp valid_sort_order(_params), do: :asc
 end
