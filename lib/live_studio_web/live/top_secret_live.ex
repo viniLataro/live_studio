9e374b38eecba5b9c9a316383de1ab1965878312
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-03-24 19:02:29 -0300

    add top secret live requiring authenticated user

---
+++ lib/live_studio_web/live/top_secret_live.ex
@@ -0,0 +1,31 @@
+defmodule LiveStudioWeb.TopSecretLive do
+  use LiveStudioWeb, :live_view
+
+  on_mount {LiveStudioWeb.UserAuth, :ensure_authenticated}
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div id="top-secret">
+      <img src="/images/spy.svg" />
+      <div class="mission">
+        <h1>Top Secret</h1>
+        <h2>Your Mission</h2>
+        <h3><%= pad_user_id(@current_user) %></h3>
+        <p>
+          Storm the castle and capture 3 bottles of Elixir.
+        </p>
+      </div>
+    </div>
+    """
+  end
+
+  defp pad_user_id(user) do
+    user.id
+    |> Integer.to_string()
+    |> String.pad_leading(3, "0")
+  end
+end
