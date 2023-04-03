62c94278fefe60c1c4ae41843f28f1f623e7c6bf
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-04-03 16:30:01 -0300

    add JS commands to show-cart client side

--- lib/live_studio_web/live/app/shop_live.ex
+++ lib/live_studio_web/live/app/shop_live.ex
@@ -2,6 +2,7 @@ defmodule LiveStudioWeb.Live.App.ShopLive do
   use LiveStudioWeb, :live_view
 
   alias LiveStudio.Products
+  alias Phoenix.LiveView.JS
 
   def mount(_params, _session, socket) do
     {:ok,
@@ -12,13 +13,31 @@ defmodule LiveStudioWeb.Live.App.ShopLive do
      )}
   end
 
-  def handle_event("toggle-show-cart", _, socket) do
-    socket = update(socket, :show_cart, fn show -> !show end)
-    {:noreply, socket}
-  end
-
   def handle_event("add-product", %{"product" => product}, socket) do
     cart = Map.update(socket.assigns.cart, product, 1, &(&1 + 1))
     {:noreply, assign(socket, :cart, cart)}
+  end
+
+  def toggle_cart(js \\ %JS{}) do
+    js
+    |> JS.toggle(
+      to: "#cart",
+      in: {
+        "ease-in-out duration-300",
+        "translate-x-full",
+        "translate-x-0"
+      },
+      out: {
+        "ease-in-out duration-300",
+        "translate-x-0",
+        "translate-x-full"
+      },
+      time: 300
+    )
+    |> JS.toggle(
+      to: "#backdrop",
+      in: "fade-in",
+      out: "fade-out"
+    )
   end
 end
